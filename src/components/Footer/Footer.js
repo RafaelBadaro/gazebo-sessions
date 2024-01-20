f9d35eded88932a96fdf32a03be14cef6377ff9c
RafaelBadaro <rafabadaro.15@gmail.com>
2024-01-20 20:11:11 -0300

    creating footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,13 @@
-
+import './Footer.css';
 
 function Footer() {
-    return (
-      <div>
-  <h1>footer</h1>
+  return (
+    <div className="footer">
+      <div className="follow-us">
+        <h3>Foolow us: @gazebosessions</h3>
       </div>
-    );
-  }
-  
-  export default Footer;
-  
+    </div>
+  );
+}
+
+export default Footer;
